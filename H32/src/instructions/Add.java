1cf00cd7fe1bc3a7ec1f008cd5421e0e019d92b7
R. Alan Whitehurst <alan.whitehurst@svu.edu>
2018-02-27 11:36:18 -0500

    cleaned up inports

--- H32/src/instructions/Add.java
+++ H32/src/instructions/Add.java
@@ -1,25 +1,17 @@
 package instructions;
 
 import h32.Executable;
-import h32.H32;
-import static h32.H32.*;
-import static h32.RegisterManipulator.CY;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author pcctoo
  */
 public class Add implements Executable {
-
-    // case 0x02:
+    
     @Override
     public void execute(int[] regs, int[] mem, int mar, boolean trace) {
         //AC = AC + mem{X}; CY = CARRY
+        // case 0x02:
         long result = (long) regs[AC] + (long) mem[mar];
         regs[AC] = (int) result;
         if (result > 0xFFFFFFFFL) {
