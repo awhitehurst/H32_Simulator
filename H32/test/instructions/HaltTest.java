1b167dd3f04790da166d8d5833c983ac9969f47e
Norcal23 <joshuakneilson@gmail.com>
2018-02-28 21:23:17 -0500

    update

--- H32/test/instructions/HaltTest.java
+++ H32/test/instructions/HaltTest.java
@@ -1,45 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package instructions;
 
 import h32.H32;
 import h32.RegisterManipulator;
 import static h32.RegisterManipulator.AC;
-import static h32.RegisterManipulator.BP;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 /**
  *
- * @author Joshua
+ * @author Joshua Neilson
  */
-public class AinTest implements RegisterManipulator {
-
+public class HaltTest implements RegisterManipulator {
+    
     private final H32 sim = new H32();
-    private final Ain inst = new Ain();
+    private final Halt inst = new Halt();
 
-    public AinTest() {
+    public HaltTest() {
 
     }
 
     /**
-     * Test of execute method, of class Ain.
+     * Test of execute method, of class Flip.
      */
     @Test
     public void testExecute() {
         System.out.println("execute");
         int[] regs = sim.getRegs();
         int[] mem = sim.getMem();
+        regs[AC] = -2;
         int mar = 0;
         boolean trace = false;
-        Ain instance = inst;
+        Halt instance = inst;
         instance.execute(regs, mem, mar, trace);
         int result = regs[AC];
         System.out.println(result);
-        // assertEquals(result,0);
+        assertEquals(result, null);
     }
-
+    
 }
