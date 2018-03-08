b8f0f37773d917c56c65687e464bef2d304170d0
Norcal23 <joshuakneilson@gmail.com>
2018-03-07 20:26:38 -0500

    jatest update

--- H32/test/instructions/JaTest.java
+++ H32/test/instructions/JaTest.java
@@ -7,6 +7,8 @@ package instructions;
 
 import h32.H32;
 import h32.RegisterManipulator;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 /**
  *
@@ -16,5 +18,25 @@ public class JaTest implements RegisterManipulator{
     
     private final H32 sim = new H32();
     private final Ja inst = new Ja();
+    
+    public JaTest(){
+        
+    }
+    
+    /**
+     * Test of execute method, of class Ja.
+     */
+    
+    @Test
+    public void testExecute(){
+        System.out.println("execute Ja");
+        int [] mem = sim.getMem();
+        int [] regs = sim.getRegs();
+        int mar = 10;
+        boolean trace = false;
+        Ja instance = inst;
+        instance.execute(regs, mem, mar, trace);
+        assertEquals(regs[PC],10);
+    }
     
 }
