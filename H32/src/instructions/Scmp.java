f386ba637a3fb2cf6085f6f070c464743e2b01ac
Brunner285 <alexander.brunner@students.svu.edu>
2018-03-20 11:48:37 -0400

    work
    work

--- H32/src/instructions/Scmp.java
+++ H32/src/instructions/Scmp.java
@@ -7,7 +7,7 @@ import h32.Executable;
  *
  * @author Joshua Neilson
  */
-public class Scpm implements Executable {
+public class Scmp implements Executable {
 
     @Override
     public void execute(int[] regs, int[] mem, int mar, boolean trace) {
